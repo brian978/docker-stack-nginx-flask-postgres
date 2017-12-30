9630af133799ac5e490249cd3b45af0c1795b279
brian978 <andrei.dincescu1@gmail.com>
2017-12-30 22:29:03 +0200

    Updated the uwsgi run file to fix cross OS compatibilty issues

--- app/scripts/docker/run-uwsgi.sh
+++ app/scripts/docker/run-uwsgi.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ ! -d "/var/app/venv" ]; then
-    cp -R /var/app_default/venv /var/app/venv
-
-    echo "Deploying default virtual environment"
-fi
-
-/var/app/venv/bin/uwsgi --socket python:8282 --uid www-py --enable-threads --master --wsgi-file=/var/app/app.py --callable app
+/var/app_docker_env/venv/bin/uwsgi --socket python:8282 --uid www-py --enable-threads --master --wsgi-file=/var/app/app.py --callable app

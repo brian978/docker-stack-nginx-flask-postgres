5cd0b1ce63a3fec6b6c64c8a8aa5ad729fb09d68
brian978 <andrei.dincescu1@gmail.com>
2017-12-31 09:53:16 +0200

    Updated the file structure

--- app/scripts/docker/run-uwsgi.sh
+++ app/scripts/docker/run-uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/var/app_docker_env/venv/bin/uwsgi --socket python:8282 --uid www-py --enable-threads --master --wsgi-file=/var/app/app.py --callable app
+/var/app_docker_env/venv/bin/uwsgi --socket python:8282 --uid www-py --enable-threads --master --wsgi-file=/var/app/run.py --callable app
